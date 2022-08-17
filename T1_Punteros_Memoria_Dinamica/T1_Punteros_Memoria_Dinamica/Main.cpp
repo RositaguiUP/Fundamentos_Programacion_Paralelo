628f9b904cfa6c21ad654c6b1501b715ca6384d9
Rosita <65567638+RositaguiUP@users.noreply.github.com>
2022-08-17 09:48:01 -0500

    Ejercicio 3 y 4

--- T1_Punteros_Memoria_Dinamica/T1_Punteros_Memoria_Dinamica/Main.cpp
+++ T1_Punteros_Memoria_Dinamica/T1_Punteros_Memoria_Dinamica/Main.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 void exc3();
+void exc4();
 float average(float*);
+void addMatrixes(int, int*, int*, int*);
 
 int main() {
-	exc3();
+	exc4();
 }
-
 
 void exc3() {
 	float* temp, * avg;
@@ -26,10 +27,45 @@ void exc3() {
 
 	*avg = average(temp);
 
-	cout << "\nEl promedio es " << avg;
+	cout << "\nEl promedio es " << *avg;
 
 	free(temp);
 	free(avg);
+}
+
+void exc4() {
+	int i, j, size, totalSize, *matA, *matB, * matC;
+
+	cout << "Ingrese el numero del tamaño de las matrices: ";
+	cin >> size;
+
+	totalSize = size * size;
+
+	matA = (int*)malloc(sizeof(int) * totalSize);
+	matB = (int*)malloc(sizeof(int) * totalSize);
+	matC = (int*)malloc(sizeof(int) * totalSize);
+
+	cout << "\nIngrese los elementos de la matriz 1\n";
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			cout << "pos[" << i << "][" << j << "]: ";
+			cin >> matA[i*size + j];
+		}
+	}
+
+	cout << "\nIngrese los elementos de la matriz 2\n";
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			cout << "pos[" << i << "][" << j << "]: ";
+			cin >> matB[i*size + j];
+		}
+	}
+
+	addMatrixes(size, matA, matB, matC);
+
+	free(matA);
+	free(matB);
+	free(matC);
 }
 
 float average(float* temp) {
@@ -41,4 +77,39 @@ float average(float* temp) {
 	cout << suma / 7 << endl;
 
 	return suma / 7;
+}
+
+void addMatrixes(int size, int* a, int* b, int* c) {
+	int i, j;
+
+	for (i = 0; i < size * size; i++) {
+		c[i] = a[i] + b[i];
+	}
+	
+	cout << "\nLa suma de matrices es:\n";
+
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			cout << "\t" << a[i* size + j];
+		}
+		cout << endl;
+	}
+
+	cout << "\nLa suma de matrices es:\n";
+
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			cout << "\t" << b[i * size + j];
+		}
+		cout << endl;
+	}
+
+	cout << "\nLa suma de matrices es:\n";
+
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			cout << "\t" << c[i * size + j];
+		}
+		cout << endl;
+	}
 }
